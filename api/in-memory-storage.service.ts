35c84be6d4bb429a16ce015a00df6471c8fb2edf
Margarita Krutikova <margaretkru@gmail.com>
2016-12-11 14:11:18 +0100

    Added more test data in the in-memory storage.

--- api/in-memory-storage.service.ts
+++ api/in-memory-storage.service.ts
@@ -5,21 +5,48 @@ export class InMemoryStorageService {
         let languageNotes = [
             { 
                 "id": 1, 
+                "note": "att få sparken från jobbet", 
+                "translations": ["to get fired"] 
+            },
+            { 
+                "id": 2, 
+                "note": "knarka, knarkigt", 
+                "translations": ["do drugs"] 
+            },
+            { 
+                "id": 3, 
+                "note": "att lagra, lagring", 
+                "translations": ["to store, storage"] 
+            },
+            { 
+                "id": 4, 
+                "note": "att ta i som ett djur", 
+                "explanations": ["(c) Anders, prat om marklyft"] 
+            },
+            { 
+                "id": 5, 
+                "note": "mottagning", 
+                "translations": ["reception"],
+                "explanations": ["mobiltäckning"],
+                "usages": [ "dålig mottagning (telefon)" ]
+            },
+            { 
+                "id": 6, 
                 "note": "tillägna", 
                 "usages": ["jag ska tillägna dagen med att"] 
             },
             { 
-                "id": 2, 
+                "id": 7, 
                 "note": "inspirerad", 
                 "usages" : ["jag är inspirerad av någon"] 
             },
             { 
-                "id": 3, 
+                "id": 8, 
                 "note": "jag ska tillägna dagen med att", 
                 "translations": ["i will make use of the day by doing"] 
             },
             { 
-                "id": 4, 
+                "id": 9, 
                 "note": "att tillägna något",
                 "explanations":  ["ta något tillgodo", "to make use of" ] 
             }
